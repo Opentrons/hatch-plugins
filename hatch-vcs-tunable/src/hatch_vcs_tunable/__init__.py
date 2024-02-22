102d93c5a424e84df935a06c97d771c73c1ed733
Seth Foster <seth@opentrons.com>
2024-02-22 13:29:40 -0500

    hatch-vcs-tunable: feat: new project (#1)
    The hatch-vcs-tunable hook allows you to override settings passed to
    hatch_vcs (and to setuptools_scm from there) via environment variables,
    useful when working on a project that has multiple release tracks with
    different versions.

---
+++ hatch-vcs-tunable/src/hatch_vcs_tunable/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present Opentrons Engineering <engineering@opentrons.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from ._version import __version__, __version_tuple__
+
+__ALL__ = ['__version__', '__version_tuple__']
